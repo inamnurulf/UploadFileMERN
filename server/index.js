2b534ef5965f23648a56ce3b6132aeb17133d867
inamnurul <aam.inam7310@gmail.com>
2024-01-17 19:08:51 +0700

    feat: read file

--- server/index.js
+++ server/index.js
@@ -19,14 +19,16 @@ app.use(cors())
 
 app.post("/upload", upload.single("file"), (req, res) => {
   const fileBuffer = req.file.buffer;
-  const filename = req.file.originalname;
+
+  // Generate a timestamp to be used as the filename
+  const timestamp = new Date().getTime();
+  const filename = `${timestamp}_${req.file.originalname}`;
 
   const bufferStream = new stream.PassThrough();
   bufferStream.end(fileBuffer);
 
   const uploadStream = bucket.openUploadStream(filename, {
-    chunkSizeBytes: 1024 * 1024,
-    metadata: { field: "myField", value: "myValue" },
+    chunkSizeBytes: 1024 * 1024
   });
 
   bufferStream.pipe(uploadStream);
@@ -42,20 +44,47 @@ app.post("/upload", upload.single("file"), (req, res) => {
 });
 
 app.get("/file/:filename", async (req, res) => {
-    try {
-      const file = await bucket.find({ filename: req.params.filename });
-  
-      if (!file) {
-        return res.status(404).json({ message: "File not found" });
+  try {
+    const file = bucket.find({ filename: req.params.filename });
+
+    let fileFound = false;
+
+    for await (const doc of file) {
+      if (doc) {
+        const downloadStream = bucket.openDownloadStreamByName(req.params.filename);
+        downloadStream.pipe(res);
+        fileFound = true;
+        break; // Stop the loop once the file is found
       }
-  
-      const downloadStream = bucket.openDownloadStreamByName(req.params.filename);
-      downloadStream.pipe(res);
-    } catch (err) {
-      console.error(err);
-      res.status(500).json({ message: "Internal Server Error" });
     }
-  });
+
+    if (!fileFound) {
+      return res.status(404).json({ message: "File not found" });
+    }
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+app.get("/allFiles", async (req, res) => {
+  try {
+    const files = await bucket.find({}).toArray();
+
+    if (!files || files.length === 0) {
+      return res.status(404).json({ message: "No files found" });
+    }
+
+    const fileNames = files.map(file => file.filename);
+    res.status(200).json({ fileNames });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
   
 
 app.listen(port, () => {
